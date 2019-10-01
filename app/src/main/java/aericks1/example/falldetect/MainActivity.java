0835816840c68edd8fa5c3b77861375bed827d9a
annae44 <aericks1@uvm.edu>
2019-10-01 11:01:10 -0400

    Initial commit

---
+++ app/src/main/java/aericks1/example/falldetect/MainActivity.java
@@ -0,0 +1,14 @@
+package aericks1.example.falldetect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
