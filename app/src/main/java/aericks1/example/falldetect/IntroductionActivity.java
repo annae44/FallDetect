4af03ff334ccf6b6c54a0fc81ef0db1061c52297
annae44 <aericks1@uvm.edu>
2019-10-03 10:49:07 -0400

    Introduction Page - Start Button

--- app/src/main/java/aericks1/example/falldetect/IntroductionActivity.java
+++ app/src/main/java/aericks1/example/falldetect/IntroductionActivity.java
@@ -2,7 +2,7 @@ package aericks1.example.falldetect;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-2
+
 public class IntroductionActivity extends AppCompatActivity {
 
     @Override
