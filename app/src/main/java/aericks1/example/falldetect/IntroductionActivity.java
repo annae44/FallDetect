553b8acec59547592b1d83826cd44cea6348c562
annae44 <aericks1@uvm.edu>
2019-10-03 10:43:09 -0400

    Introduction Page Added

---
+++ app/src/main/java/aericks1/example/falldetect/IntroductionActivity.java
@@ -0,0 +1,14 @@
+package aericks1.example.falldetect;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+2
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction);
+    }
+
+}
--- app/src/main/java/aericks1/example/falldetect/MainActivity.java
+++ app/src/main/java/aericks1/example/falldetect/MainActivity.java
@@ -2,13 +2,33 @@ package aericks1.example.falldetect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.true_button);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                // open new activity using an intent
+                Intent IntroIntent = new Intent(getApplicationContext(), IntroductionActivity.class);
+                startActivity(IntroIntent);
+
+            }
+        });
+
     }
 }
