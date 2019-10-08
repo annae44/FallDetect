e2100f47393a8a55ccbe3ddedae7c2e8907c2154
huanganyi <anyi.huang@uvm.edu>
2019-10-08 10:17:17 -0400

    prepareActivity

---
+++ app/src/main/java/aericks1/example/falldetect/PrepareActivity.java
@@ -0,0 +1,12 @@
+package aericks1.example.falldetect;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class PrepareActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prepare);
+    }
+}
+
