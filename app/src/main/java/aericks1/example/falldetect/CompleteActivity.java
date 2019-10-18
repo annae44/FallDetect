845d69c566083d5dd18db4806b2dd84373747ff6
annae44 <aericks1@uvm.edu>
2019-10-18 09:58:38 -0400

    Accelerometer Collection with bugs

--- app/src/main/java/aericks1/example/falldetect/CompleteActivity.java
+++ app/src/main/java/aericks1/example/falldetect/CompleteActivity.java
@@ -1,8 +1,12 @@
 package aericks1.example.falldetect;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CompleteActivity extends AppCompatActivity {
@@ -11,6 +15,8 @@ public class CompleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.complete);
+
+        sendEmail();
 
         mStartAgain = (Button) findViewById(R.id.start_again_button);
 
@@ -25,6 +31,28 @@ public class CompleteActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {"aericks1@uvm.edu"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "FallDetect Results");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Here are the results:");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email.", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(CompleteActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
--- app/src/main/java/aericks1/example/falldetect/PrepareActivity.java
+++ app/src/main/java/aericks1/example/falldetect/PrepareActivity.java
@@ -1,5 +1,10 @@
 package aericks1.example.falldetect;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.os.Handler;
@@ -7,9 +12,15 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class PrepareActivity extends AppCompatActivity {
+public class PrepareActivity extends AppCompatActivity implements SensorEventListener {
     TextView timerTextView;
     long startTime = -1;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensorAccelerometer;
+    private TextView mSensorX;
+    private TextView mSensorY;
+    private TextView mSensorZ;
 
     boolean during = false;
     boolean done = false;
@@ -32,7 +43,9 @@ public class PrepareActivity extends AppCompatActivity {
                     startTime = (int) (System.currentTimeMillis() / 1000);
                     during = true;
                 }
-            } else if (seconds == 10){
+            } else if (seconds >= 10){
+                 onStart();
+            } else {
                 Intent CompleteIntent = new Intent(getApplicationContext(), CompleteActivity.class);
                 startActivity(CompleteIntent);
             }
@@ -46,10 +59,47 @@ public class PrepareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prepare);
 
-        System.out.println(startTime);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         timerHandler.postDelayed(timerRunnable, 0);
         timerTextView = (TextView) findViewById(R.id.timer_text_view);
+
+        mSensorX = (TextView) findViewById(R.id.sensor_text_view_x);
+        mSensorY = (TextView) findViewById(R.id.sensor_text_view_y);
+        mSensorZ = (TextView) findViewById(R.id.sensor_text_view_z);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float currentValue = sensorEvent.values[0];
+        mSensorX.setText(getResources().getString(
+                R.string.sensor_text_view_x, currentValue));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
